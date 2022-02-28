268fcca0e4a90e7f98b0f4956c7049c6afae87d4
Siddharth Goyal <siddharth.goyal@citymall.live>
2022-02-28 18:47:57 +0530

    Auth added

--- config/database.py
+++ config/database.py
@@ -1,8 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from pymongo import MongoClient
 CONNECTION_STRING = "mongodb+srv://siddharthgoyal:t2KHFCLmvshVZpN6@cluster0.fqx5j.mongodb.net/test"
 
 client = MongoClient("mongodb+srv://siddharthgoyal:t2KHFCLmvshVZpN6@cluster0.fqx5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.todo_app
-
-
 collection_name = db["todos_app"]
+c_name=db["users_app"]
